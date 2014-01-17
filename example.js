39411595e081f15773610dc94724a381c069e92c
Giannis kosmas <kosmas@lib.uoc.gr>
2014-01-17 13:17:19 +0200

    Allow and handle message passing between master and workers

--- example.js
+++ example.js
@@ -2,15 +2,39 @@
 
 var pluribus = require('./lib/pluribus.js');
 
-function worker() {
-    console.log("I'm a worker");
+function workerMessageHandler(msg) {
+  switch (msg) {
+    case "test" : console.log('Got message "'+msg+'" from master.');
+                  break;
+        default : break;
+  }
 }
 
-function master() {
+function masterMessageHandler(msg) {
+  switch (msg) {
+    case "hello" : console.log('Got message "'+msg+'" from worker.');
+                  break;
+        default : break;
+  }
+}
+
+function worker(master) {
+    console.log("I'm a worker");
+    setTimeout(function() {master.send("hello");}, 3000);
+}
+
+function master(workers) {
     console.log("I'm the master");
+    process.on('SIGUSR2', function () {
+      console.log("Master got SIGUSR2");
+      for (var id in workers) {
+        workers[id].send('test');
+      }
+    });
 }
 
 pluribus.execute("Example", {
     "master": master,
-    "worker": worker
+    "worker": worker,
+    "msgHandler": {"worker" : workerMessageHandler, "master": masterMessageHandler}
 });
